0860c460f66c04d74ca1802df9370ac8f5f66290
Juan J Guirao Garcia <juan.jose.guirao@gmail.com>
2022-04-13 14:17:56 +0100

    building sidebar and first pages

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,24 @@
+import "./sidebar.css";
+import {
+    LineStyle,
+    Timeline,
+    TrendingUp,
+    PermIdentity,
+    Storefront,
+    AttachMoney,
+    BarChart,
+    MailOutline,
+    DynamicFeed,
+    ChatBubbleOutline,
+    WorkOutline,
+    Report,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <div className='sidebarWrapper'></div>
+        </div>
+    );
+}
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import "./topbar.css";
-import { NotificationsNone } from "@mui/icons-material";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material";
 
 export default function Topbar() {
     return (
         <div className='topbar'>
             <div className='topbarWrapper'>
                 <div className='topLeft'>
-                    <span className='logo'>Minerva Dashboard</span>
+                    <span className='logo'>Minerva Apps</span>
                 </div>
                 <div className='topRight'>
                     <div className='topbarIconContainer'>
                         <NotificationsNone fontSize='large' />
                         <span className='topIconBadge'>2</span>
                     </div>
+                    <div className='topbarIconContainer'>
+                        <Language fontSize='large' />
+                        <span className='topIconBadge'>2</span>
+                    </div>
+                    <div className='topbarIconContainer'>
+                        <Settings fontSize='large' />
+                    </div>
+                    <img
+                        src='https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg'
+                        alt=''
+                        className='topAvatar'
+                    />
                 </div>
             </div>
         </div>
